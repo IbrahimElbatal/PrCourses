46c9fea16e1dc1e87497eae7d58832f28db3c07d
ibrahim elbatal <ibrahimelbatal2@gmail.com>
2020-06-07 20:12:31 +0200

    some applied style

--- PragimCourses/PragimCourses/App_Start/BundleConfig.cs
+++ PragimCourses/PragimCourses/App_Start/BundleConfig.cs
@@ -8,7 +8,8 @@ namespace PragimCourses
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/Isotope.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
